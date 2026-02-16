a6f022c201d423dc24f2fc5e46cb73d5f9ec57bd
mohitborhade00 <mohitborhade00@gmail.com>
2026-02-16 18:50:59 +0530

    open

--- portfolio-website/script.js
+++ portfolio-website/script.js
@@ -1,4 +1,5 @@
-const text = "Frontend Developer | BSc CS Student";
+// Typing Effect
+const text = "Frontend Developer | Computer Science Student";
 let index = 0;
 const typing = document.getElementById("typing");
 
@@ -6,8 +7,26 @@ function typeEffect() {
     if (index < text.length) {
         typing.textContent += text.charAt(index);
         index++;
-        setTimeout(typeEffect, 100);
+        setTimeout(typeEffect, 80);
     }
-}
+} 
 
 typeEffect();
+
+// Scroll Reveal
+const hiddenSections = document.querySelectorAll(".hidden");
+
+window.addEventListener("scroll", () => {
+    hiddenSections.forEach(section => {
+        const top = section.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            section.classList.add("show");
+        }
+    });
+});
+
+// Dark Mode
+document.getElementById("theme-toggle")
+.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
